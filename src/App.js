b5d218983c97a201f1896e57c36740f9dac25202
Alex-Viet <alex.night.power@gmail.com>
2023-11-24 13:54:03 +0300

    Причесал основные файлы

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import * as React from 'react'
-import { Profile } from './pages/profile/profile'
 import { AppRoutes } from './routes'
-import { useState } from 'react';
-import { WorkoutInfoPage } from './pages/WorkoutInfoPage/WorkoutInfoPage'
-import { GlobalStyle, Wrapper } from './index.styles'
-import { AuthPage } from './pages/AuthPage/Auth'
-import { MainPage } from './pages/MainPage/MainPage'
-import { WorkoutVideoPage } from './pages/WorkoutVideoPage/WorkoutVideoPage'
 
 function App() {
-  // const [user, setUser] = useState(null);
-
-
   return (
     <>
-      <AppRoutes/>    
+      <AppRoutes />
     </>
   )
 }
 
-export default App;
-
+export default App
